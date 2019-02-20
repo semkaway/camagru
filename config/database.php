57c20302c886b477f85a1f25734fcc131321a48a
semkaway <semkaway@gmail.com>
2019-02-20 18:16:14 +0200

    [UPD] db name && username

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
-	$DB_NAME = "camagru";
+	$DB_NAME = "id8786874_camagru";
 	$DB_DSN = "mysql:host=localhost;dbname=".$DB_NAME;
 	$DB_DSN_SHORT = "mysql:host=localhost";
-	$DB_USER = 'root';
+	$DB_USER = 'id8786874_root';
 	$DB_PASSWORD = 'aaa111';
 ?>
